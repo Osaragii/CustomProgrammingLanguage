7fe9857168f704fb2962e7b033cf0ceb356269d4
Luna <kytluna.c@gmail.com>
2024-11-13 13:56:02 +0000

    Update main.cpp
    raaaaaaaahhhhhhhhhhhhhhh

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 
-using namespace std;
 int main() {
-    cout<<"Welcome to whatever we are doing!!!yayayayayy?!?!!\n";
+    std::cout<<"Welcome to whatever we are doing!!!yayayayayy?!?!!\n";
 
     return 0;
 }
